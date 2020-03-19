f35f35864adad468a2aede67ab6e87ca4d9902ad
Sabu Siyad <ssiyad@tuta.io>
2020-03-20 00:13:41 +0530

    Randomize everything

--- bot.py
+++ bot.py
@@ -1,5 +1,28 @@
+# MIT License
+
+# Copyright(c) 2020 Sabu Siyad
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from telegram.ext import Updater, MessageHandler, run_async, Filters
 from http import client
+from random import randint
 
 import config
 
@@ -7,16 +30,28 @@ updater = Updater(config.BOT_API, use_context=True)
 dp = updater.dispatcher
 
 
-def GetNum(s):
+def ToNum(s):
     try:
         return int(s)
     except ValueError:
         return False
 
-def GetRes(n):
+
+def GetRes(n, date=False):
+    cases = [f"/{n}", f"/{n}/math"]
+    if 1500 < n < 2000:
+        cases = [f"/{n}/year"]
     connection = client.HTTPConnection("numbersapi.com")
-    connection.request("GET", f"/{n}")
+    connection.request("GET", cases[randint(0, len(cases)-1)])
     return connection.getresponse().read().decode()
+
+
+def GetNum(s):
+    list = [ToNum(n) for n in s.split(" ") if ToNum(n) != False]
+    if list != []:
+        return list[randint(0, len(list)-1)]
+    else:
+        return False
 
 
 @run_async
